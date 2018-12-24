3321d3c642d878c44d479abb04871cb9258344b8
Lydi <lydiamariong@gmail.com>
2018-12-24 17:31:21 -0600

    Initial commit.

--- Seefood/ViewController.swift
+++ Seefood/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Setup for imagePicker
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera 
+        imagePicker.allowsEditing = false
+        
     }
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedimage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedimage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
 
 }
 
